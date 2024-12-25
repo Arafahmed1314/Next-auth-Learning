031afa1442c4f65773a5ecee680cd7c9cc2a9448
Naimul Islam <nayemhasan1314@gmail.com>
2024-12-25 17:58:55 +0600

    Reinitialize repository and add files

---
+++ app/components/Header.jsx
@@ -0,0 +1,6 @@
+import { auth, signIn, signOut } from "@/auth";
+export default async function Header() {
+  //   const session = await auth();
+  //   console.log(session);
+  return <div>Header</div>;
+}
